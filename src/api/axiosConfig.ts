d12fa82ed5831697212e041293ced4fc02994d60
NgocbichChu <ngocbichh2601@gmail.com>
2025-08-11 14:00:56 +0700

    update url  api server

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios, { type AxiosRequestConfig, type AxiosResponse } from "axios"
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:9999/",
-  baseURL: "https://booking-nimbus-backend-latest.onrender.com/",
+  baseURL: "http://157.66.24.112:8080/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
